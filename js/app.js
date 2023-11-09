aa9a88116439e99dc9c91139918162f27d432940
hamidreza soltani <h.soltani8452@gmail.com>
2023-11-09 16:19:50 +0330

    first code

---
+++ js/app.js
@@ -0,0 +1,10 @@
+export const x="arian";
+
+export const y="ali";
+
+
+
+
+export default function sum(x,y){
+    console.log(x+y)
+}
---
+++ js/script.js
@@ -0,0 +1,10 @@
+import sum from "./app.js";
+import {x as z} from "./app.js"
+
+const x="taghi";
+
+console.log(z);
+
+
+
+sum(45,52);
